0403c89575ec1d5495c5c73d0e34bf6aa08a3753
cantisaniag <cantisaniag@gmail.com>
2022-11-09 14:09:28 -0300

    java2

--- javascript/codigo.js
+++ javascript/codigo.js
@@ -1,26 +1,13 @@
 
-
-
-
-
-
 function myFunction(totalAPagar, precioCantidad, descuento) {
-  var cantidad = document.getElementById("cant").value;
   var ticket = 200;
+  var cantidad = document.getElementById("cant").value;
   var precioCantidad = ticket * cantidad;
-  var categoria = 0.8;
+  var categoria = document.getElementById("categoria1").value;
   var descuento = precioCantidad * categoria;
   var totalAPagar = precioCantidad - descuento;
-  let text = document.getElementById("titulo").innerHTML; 
-  document.getElementById("titulo").innerHTML =
-  text.replace("$", totalAPagar);
-  console.log(precioCantidad);
-  console.log(descuento);
+  document.getElementById("titulo").value= "Total a pagar: $" + totalAPagar
+  console.log("precio x cantidad " +(precioCantidad));
+  console.log("descuento de $ " +(descuento));
   console.log("total a pagar:$" +(totalAPagar));
 }
-
-
-
-
-
-
